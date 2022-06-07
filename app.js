1f699d854c6807cfe0e0055b1dc45054e39a0f32
RSMarian <marianstefan1499@gmail.com>
2022-06-07 22:41:21 +0300

    populated items dynamically

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//items array
 const menu = [
   {
     id: 1,
@@ -72,3 +73,25 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function(){
+  let displayMenu = menu.map(function(item){
+
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.title}>
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">${item.price}</h4>
+      </header>
+      <p class="item-text">${item.desc}</p>
+    </div>
+  </article>`;
+  });
+
+  displayMenu = displayMenu.join("");
+
+  sectionCenter.innerHTML = displayMenu;
+});
