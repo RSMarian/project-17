49958987abb2e6266679cc225f50e51a1053abab
RSMarian <marianstefan1499@gmail.com>
2022-06-08 14:29:53 +0300

    moved functionality inside a function

--- app.js
+++ app.js
@@ -77,7 +77,11 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function(){
-  let displayMenu = menu.map(function(item){
+  displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
 
     return `<article class="menu-item">
     <img src=${item.img} class="photo" alt=${item.title}>
@@ -94,4 +98,4 @@ window.addEventListener('DOMContentLoaded', function(){
   displayMenu = displayMenu.join("");
 
   sectionCenter.innerHTML = displayMenu;
-});
+};
